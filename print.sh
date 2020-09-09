729cf4042ecc4daf8713e2b2fb96fec518c5a208
devops <md_syd01@yahoo.co.in>
2020-09-09 15:47:40 +0530

    print.sh

---
+++ print.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Author: Mohammed sayeeduddin
+#Date: 08-09-2020
+#Description: This script will provide the simple hello world on the screen
+#Modified Date: 08-09-2020
+echo -e "Welcome to shell scripting\n\tThis is from devops training"
