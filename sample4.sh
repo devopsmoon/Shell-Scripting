a0fbb9f3e4451320c7091bc2a142eef14bd828a0
devops <md_syd01@yahoo.co.in>
2020-09-09 19:34:12 +0530

    mycommit102

--- sample4.sh
+++ sample4.sh
@@ -3,4 +3,4 @@
 #Date: 09-09-20202
 #Description: This script will provide the sample color
 echo -e "\e[1mWelcome To Devops world\e[0m"
-echo -e "\e[14mWelcome To Shell Scripting world\e[0m"
+echo -e "\e[1;4mWelcome To Shell Scripting world\e[0m"
