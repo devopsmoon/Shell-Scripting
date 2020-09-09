2289507be55868f6027d60cc1056d39d406344d4
devops <md_syd01@yahoo.co.in>
2020-09-09 19:32:29 +0530

    mycommit101

---
+++ sample4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Author: Mohammed Sayeeduddin
+#Date: 09-09-20202
+#Description: This script will provide the sample color
+echo -e "\e[1mWelcome To Devops world\e[0m"
+echo -e "\e[14mWelcome To Shell Scripting world\e[0m"
