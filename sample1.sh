5b4ce061618b9d5188fd4339e8f93cfecc09d3d0
devops <md_syd01@yahoo.co.in>
2020-09-09 18:23:48 +0530

    sample1.sh

---
+++ sample1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Author: Mohammed Sayeeduddin
+#Date: 09090-2020
+#Description: This script will provide the echo syntax
+echo "Welcome to Devops world/n/tThis is shell scripting Training"
