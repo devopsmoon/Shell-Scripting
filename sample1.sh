b1a43e25c94eba066d88d0ebeff2f2f13671e443
devops <md_syd01@yahoo.co.in>
2020-09-09 18:29:03 +0530

    sample1.1.1.sh

--- sample1.sh
+++ sample1.sh
@@ -2,4 +2,4 @@
 #Author: Mohammed Sayeeduddin
 #Date: 09090-2020
 #Description: This script will provide the echo syntax
-echo -e "Welcome to Devops world/n/tThis is shell scripting Tgit raining"
+echo -e "Welcome to Devops world\n\tThis is shell scripting Training"
